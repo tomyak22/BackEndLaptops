c93160e749196dac12522db7a1082e4d1423a89b
Cam Fournier <cam.codes.15@gmail.com>
2020-03-24 16:36:53 -0400

    add functionality working

--- LaptopsService/routes/index.js
+++ LaptopsService/routes/index.js
@@ -48,71 +48,21 @@ router.get('/laptops/all/:location', (request, response, next) => {
 });
 
 router.post('/laptops/add', function (request, response) {
-  console.log(request.body);      // your JSON
-
+  console.log('in add');
   
-
   fs.readFile('./data/laptops.json', function (err, data) {
     var object = request.body;
+    object = JSON.stringify(object);
+    object = JSON.parse(object);
     var json = JSON.parse(data);
     json.push(object);
-
-    console.log("object = " + object);
 
     fs.writeFile('./data/laptops.json', JSON.stringify(json), function (err) {
       if (err) return console.log(err);
     });
   });
-
-  //var fileContentsObject = JSON.parse('./data/laptops.json');
-  //console.log(fileContentsObject);
-
-  //var fileContents = fs.readFileSync('./data/laptops.json');
-  // fileContents = JSON.parse(fileContents);
-
-  // fileContents = JSON.stringify(fileContents);
-
-
-  // fileContents += object;
-
-  // fs.writeFile('./data/laptops.json', fileContents, function (err) {
-  //     if (err) return console.log(err); 
-  // });
-
-  // console.log(fileContents);
-  // fs.readFile('./data/laptops.json', function (err, object) {
-  //   console.log("Reading JSON");
-  //   var json = JSON.parse(object);
-  //   json.push(object);
-
-  //   fs.writeFile('./data/laptops.json', JSON.stringify(json))
-  // })
-
-  //var obj = read_json_file();
-  // console.log('json read file produces: ' + obj);
-  //obj.push(request.body);
-  //console.log(obj);
-  //  var json = JSON.stringify(request.body);
-  //json.concat(obj)
-  //fs.writeFile('./data/laptops.json', obj, function (err) {
-  //  if (err) return console.log(err);
-  //});
-
-  response.send("Success!");    // echo the result back
+  
+  response.send("Success!");
 });
-
-// router.post('/laptops/add', function (request, response) {
-//   console.log('got into add');
-//   console.log('request body: ' + request.body);
-//   fs.readFile('./data/laptops.json', 'utf8', function readFileCallback(err, data){
-//     if (err){
-//         console.log(err);
-//     } else {
-//     obj = JSON.parse(data); //now it an object
-//     obj.push(request.body); //add some data
-//     json = JSON.stringify(obj); //convert it back to json
-//     fs.writeFile('./data/laptops.json', json, 'utf8'); // write it back 
-// }});
-// });
 
 module.exports = router;
